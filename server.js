c9002425d38a71578d2e5212be1e527612755a7d
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-10 09:48:55 -0700

    added cors support

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const projectRouter = require("./projects/projectRouter");
 const actionRouter = require("./actions/actionRouter");
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "hello" });
