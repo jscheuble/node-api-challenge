b89c7688574185de6c29debc4dfd4a547b6157e8
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-10 08:52:01 -0700

    finished endpoints for projectRouter

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -13,13 +13,21 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", validateProjectId, (req, res) => {
+  db.get(req.params.id)
+    .then((proj) => {
+      res.status(200).json(proj);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "unable to retrieve project" });
+    });
+});
+
 router.post("/", (req, res) => {
   if (req.body.name === "" || req.body.description === "") {
-    res
-      .status(200)
-      .json({
-        message: "please provide a name and description for the project",
-      });
+    res.status(200).json({
+      message: "please provide a name and description for the project",
+    });
   }
 
   db.insert(req.body)
@@ -32,5 +40,40 @@ router.post("/", (req, res) => {
         .json({ message: "unable to add this project to the database" });
     });
 });
+
+router.delete("/:id", validateProjectId, (req, res) => {
+  db.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: "successfully deleted, adios" });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "unable to delete" });
+    });
+});
+
+router.put("/:id", validateProjectId, (req, res) => {
+  if (req.body.name === "" || req.body.description === "") {
+    res.status(200).json({
+      message: "please provide a name and description for the project",
+    });
+  }
+  db.update(req.params.id, req.body)
+    .then((proj) => {
+      res.status(200).json({ message: "successfully updated!", proj });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "unable to update" });
+    });
+});
+
+function validateProjectId(req, res, next) {
+  db.get(req.params.id)
+    .then((proj) => {
+      proj ? next() : res.status(400).json({ message: "invalid project id" });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "invalid project id" });
+    });
+}
 
 module.exports = router;
